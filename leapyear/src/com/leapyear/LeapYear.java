6bbb02e15759e3c0f553c40af3da40058724d170
divyanaC999 <divyachourasiya999@gmail.com>
2021-12-20 16:42:13 +0530

    day 5

---
+++ leapyear/src/com/leapyear/LeapYear.java
@@ -0,0 +1,24 @@
+package com.leapyear;
+import java.util.*;
+public class LeapYear {
+
+	public static void main(String[] args) {
+				System.out.println("choose year");
+				
+				Scanner scYear = new Scanner(System.in);
+				
+				int year = scYear.nextInt();
+				scYear.close();
+				
+				if(year > 999 && year <= 9999) {
+					 if((year % 4 == 0 && year % 100 != 0) || year % 400 == 0){
+							System.out.println("Year is leap year");
+					 }else {
+							System.out.println("Not a leap year");
+					 }
+				}else {
+					System.out.println("Please enter correct year");
+				}
+	}
+
+}
